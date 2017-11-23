d42aa6f8c641b240b6438547379ab28c33b68afa
tomasz-piotrowicz <pantomash@gmail.com>
2017-11-23 22:50:25 +0100

    15.3

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -16,7 +17,15 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTask(long id){
+    public Optional<Task> getTask(final Long id){
         return repository.findById(id);
+    }
+
+    public Task saveTask(final Task task){
+        return repository.save(task);
+    }
+
+    public void deleteTask(final Long id) {
+        repository.deleteById(id);
     }
 }
