e9838911f654a53d5e5807a7293b300fa74c3289
tomasz-piotrowicz <pantomash@gmail.com>
2017-12-21 21:44:08 +0100

    heroku minor update

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -34,7 +34,7 @@ public class TrelloService {
     public CreatedTrelloCard createdTrelloCard(final TrelloCardDto trelloCardDto){
         CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
         ofNullable(newCard).ifPresent(card -> emailService.send(new Mail(adminConfig.getAdminMail(),
-                "", SUBJECT, "New card" + card.getName() + " has been created on your Trello account")));
+                null, SUBJECT, "New card" + card.getName() + " has been created on your Trello account")));
         return newCard;
     }
 }
