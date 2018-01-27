16451359f44fffd4df2f7c15bce8b2b170c94411
tomasz-piotrowicz <pantomash@gmail.com>
2018-01-27 15:37:46 +0100

    heroku update

---
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -0,0 +1,27 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class MailCreatorService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+    @Autowired
+    @Qualifier("templateEngine")
+    private TemplateEngine templateEngine;
+
+    public String buildTrelloCardEmail(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("button", "Visit webstite");
+        context.setVariable("admin_name", adminConfig.getAdminMail());
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+}
